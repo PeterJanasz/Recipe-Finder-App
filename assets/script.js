3836018c99e826f7c4fc98c7baa91ba1f1c2ecf9
Jdog2001 <134169267+JoshHill1@users.noreply.github.com>
2023-07-26 09:49:15 -0700

    Changed HTML project, modded CSS templt, added JS

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,16 @@
-const searchBox = document.querySelector('#searchBox');
-const filterBox = document.querySelector('#filter');
-const activityLog = document.querySelector('#activities');
+var removeClass = document.querySelector('.hideIt');
+
+var categoryTags = document.querySelector('.categoryAtag');
+categoryTags.addEventListener('click', showHide);
+function showHide() {
+
+  removeClass.classList.remove('hideIt');
+  
+};
+
+
+
+
+// const searchBox = document.querySelector('#searchBox');
+// const filterBox = document.querySelector('#filter');
+// const activityLog = document.querySelector('#activities');
