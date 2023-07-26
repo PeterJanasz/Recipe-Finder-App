c21e1f028594690882537227ca14024e854199c3
Weston Kostura <weston.kostura@gmail.com>
2023-07-25 18:14:30 -0700

    added js variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,3 @@
-var searchBox = document.getElementById
+var searchBox = document.querySelector('#searchBox');
+var filterBox = document.querySelector('#filter');
+var activityLog = document.querySelector('#activities');
